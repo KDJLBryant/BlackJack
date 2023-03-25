3bd0c00cc0256c7d307dc296128851eb72e23eb1
indianaea <126889057+indianaea@users.noreply.github.com>
2023-03-25 11:44:16 +0000

    money grab

--- bin/BlackJack.dart
+++ bin/BlackJack.dart
@@ -7,10 +7,17 @@ import 'package:BlackJack/lib.dart';
 
 void main() {
   Welcome();
+
   int i = 12; // testing
   String card = CardNamer(i);
   print(card);
+
   List<int> theList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   Shuffler(theList);
   print(theList);
+
+  int bank = BuyIn();
+  int bet = PlaceBet(bank);
+  print("You bet $bet out of $bank.");
+
 }
